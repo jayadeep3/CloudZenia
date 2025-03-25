4ef2445103c1b6bdf7f73b933db74658d8a09115
jayadeepallu <109648525+jayadeep3@users.noreply.github.com>
2025-03-25 15:02:41 +0530

    Create main.tf

---
+++ terraform/modules/alb/main.tf
@@ -0,0 +1,14 @@
+resource "aws_lb" "ecs_alb" {
+  name               = "ecs-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb.id]
+  subnets           = var.public_subnets
+}
+
+resource "aws_lb_target_group" "ecs_tg" {
+  name     = "ecs-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+}
