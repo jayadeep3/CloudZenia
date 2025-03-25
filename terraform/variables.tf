8e8a5a1020295ed2a39c1924038bc98e9985690a
jayadeepallu <109648525+jayadeep3@users.noreply.github.com>
2025-03-25 15:08:41 +0530

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,43 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  description = "VPC CIDR block"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnets" {
+  description = "List of public subnets"
+  type        = list(string)
+}
+
+variable "private_subnets" {
+  description = "List of private subnets"
+  type        = list(string)
+}
+
+variable "cluster_name" {
+  description = "ECS cluster name"
+  type        = string
+  default     = "my-ecs-cluster"
+}
+
+variable "db_user" {
+  description = "Database username"
+  type        = string
+}
+
+variable "db_password" {
+  description = "Database password"
+  type        = string
+  sensitive   = true
+}
+
+variable "key_name" {
+  description = "SSH key name for EC2 instances"
+  type        = string
+}
